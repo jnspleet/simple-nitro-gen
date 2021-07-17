6ba2e53dbc90c601b5556addcfcac773ea304036
jnsp <81659740+jnsp-vsc@users.noreply.github.com>
2021-07-17 17:51:55 +0200

    added examples

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    webhook: "",
-    webhooktoken: "",
+    webhook: "", // Example: 865983833475252244
+    webhooktoken: "", // Example: 1QZCXDGNVPk-6WPcKoak0E7BY--bsRTZC8Yl9nq7ndmKTOpg1beoStf83pM7uE2Z8G38
 
     checksPerSecond: 0.7
 }
