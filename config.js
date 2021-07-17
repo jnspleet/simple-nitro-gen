f592850db9a9425e29081934f2a48c6060fc92b3
jnsp <81659740+jnsp-vsc@users.noreply.github.com>
2021-07-17 17:58:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     webhook: "", // Example: 865983833475252244
     webhooktoken: "", // Example: 1QZCXDGNVPk-6WPcKoak0E7BY--bsRTZC8Yl9nq7ndmKTOpg1beoStf83pM7uE2Z8G38
 
-    checksPerSecond: 0.7
+    checksPerSecond: 0.7 // How many requests it should send per second
 }
 
 
